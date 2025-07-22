8546151592129b971ff31c4e02153de0e46c0bda
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-07-22 10:35:23 +0400

    Add new field: teacher_id

--- src/entities/Subject.java
+++ src/entities/Subject.java
@@ -3,10 +3,12 @@ package entities;
 public class Subject {
     private Long id;
     private String name;
+    private Long teacherId;
 
-    public Subject(Long id, String name) {
+    public Subject(Long id, String name, Long teacherId) {
         this.id = id;
         this.name = name;
+        this.teacherId = teacherId;
     }
 
     public Long getId() {
@@ -24,6 +26,15 @@ public class Subject {
 
     public Subject setName(String name) {
         this.name = name;
+        return this;
+    }
+
+    public Long getTeacherId() {
+        return teacherId;
+    }
+
+    public Subject setTeacherId(Long teacherId) {
+        this.teacherId = teacherId;
         return this;
     }
 
