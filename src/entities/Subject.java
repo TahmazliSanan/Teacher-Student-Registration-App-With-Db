c33bf0f065164431358e6b017d045dc548429548
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-07-22 11:25:10 +0400

    Change relationship: From "One-to-many" to "Many-to-many"

--- src/entities/Subject.java
+++ src/entities/Subject.java
@@ -3,12 +3,10 @@ package entities;
 public class Subject {
     private Long id;
     private String name;
-    private Long teacherId;
 
-    public Subject(Long id, String name, Long teacherId) {
+    public Subject(Long id, String name) {
         this.id = id;
         this.name = name;
-        this.teacherId = teacherId;
     }
 
     public Long getId() {
@@ -26,15 +24,6 @@ public class Subject {
 
     public Subject setName(String name) {
         this.name = name;
-        return this;
-    }
-
-    public Long getTeacherId() {
-        return teacherId;
-    }
-
-    public Subject setTeacherId(Long teacherId) {
-        this.teacherId = teacherId;
         return this;
     }
 
